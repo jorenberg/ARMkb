b6e81a705df04fbb461009dcefc3adae707824a2
Sequømics <sequomics@users.noreply.github.com>
2016-01-02 22:44:43 +0530

    Initial commit
    --/The Heart of Test System/-- of "ARMkb®".
    karma, The Spectacular Test Runner for JavaScript.
    Help: https://github.com/karma-runner/karma
    Usage: $ karma init

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+
+/*!
+ * ARMkb® [v1.0.0]
+ * ______________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ * @date      : 01-Jan-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * ______________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "ARMkb®".
+ * ______________________________________________________________
+ */
